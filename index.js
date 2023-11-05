bf5c201e6d0d3e14a0c5b66662eb5d319fb02317
SantoSarker101 <santosarker404@gmail.com>
2023-11-06 00:55:01 +0600

    create port for server

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) =>{
+	res.send('Yummy Foods Ocean is running ')
+});
+
+app.listen(port, () => {
+	console.log(`Yummy Foods Ocean is running on port: ${port}`);
+})
